d4f0585188ca1b3129c49da62c37734550511288
thecircleisround <roundisthecircle@gmail.com>
2021-12-01 20:31:47 -0500

    initial commit

---
+++ 2021/Day_1/day_1.py
@@ -0,0 +1,15 @@
+input_file = list(map(int,open('input.txt').read().splitlines()))
+
+def part_one(input_file:list):
+    increases = [y for x,y in enumerate(input_file[1:]) if input_file[x] < y]
+    return len(increases)
+
+def part_two(input_file):
+    sliding_sums = [sum(input_file[i-3:i]) for i in range(3,len(input_file)+1)]
+    return part_one(sliding_sums)
+
+print(part_one(input_file))
+print(part_two(input_file))
+
+
+
